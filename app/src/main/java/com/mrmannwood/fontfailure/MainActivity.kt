9bca0da30d2485063fa708a9c448d67202f10f77
Marshall Mann-Wood <marshallmannwood@gmail.com>
2021-08-23 13:29:53 -0700

    added more fonts

--- app/src/main/java/com/mrmannwood/fontfailure/MainActivity.kt
+++ app/src/main/java/com/mrmannwood/fontfailure/MainActivity.kt
@@ -5,47 +5,91 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.provider.FontRequest
 import androidx.core.provider.FontsContractCompat
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private val FONTS = arrayOf(
+            "Roboto",
+            "Besley",
+            "Atkinson Hyperlegible",
+            "Urbanist",
+            "Open Sans",
+            "Lato",
+            "Oswald",
+            "Raleway"
+        )
+    }
+
+    private lateinit var console: TextView
+    private lateinit var handler: Handler
+
+    private var fontIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val console = findViewById<TextView>(R.id.console)
+        console = findViewById(R.id.console)
+        handler = Handler(Looper.getMainLooper())
 
+        findViewById<Button>(R.id.button).setOnClickListener {
+            requestNextFont()
+        }
+
+        requestNextFont()
+    }
+
+    private fun requestNextFont() {
+        val font = getNextFont()
         FontsContractCompat.requestFont(
             this@MainActivity,
             FontRequest(
                 "com.google.android.gms.fonts",
                 "com.google.android.gms",
-                "Atkinson Hyperlegible",
+                font,
                 R.array.com_google_android_gms_fonts_certs
             ),
-            object : FontsContractCompat.FontRequestCallback() {
-                override fun onTypefaceRetrieved(typeface: Typeface) {
-                    console.typeface = typeface
-                    console.text = "Font Request Succeeded"
-                }
-                override fun onTypefaceRequestFailed(reason: Int) {
-                    val r = when(reason) {
-                        FAIL_REASON_PROVIDER_NOT_FOUND -> "FAIL REASON PROVIDER NOT FOUND"
-                        FAIL_REASON_FONT_LOAD_ERROR -> "FAIL REASON FONT LOAD ERROR"
-                        FAIL_REASON_FONT_NOT_FOUND -> "FAIL REASON FONT NOT FOUND"
-                        FAIL_REASON_FONT_UNAVAILABLE -> "FAIL REASON FONT UNAVAILABLE"
-                        FAIL_REASON_MALFORMED_QUERY -> "FAIL REASON MALFORMED QUERY"
-                        FAIL_REASON_WRONG_CERTIFICATES -> "FAIL REASON SECURITY VIOLATION"
-                        FAIL_REASON_SECURITY_VIOLATION -> "FAIL REASON SECURITY VIOLATION"
-                        else -> "OTHER REASON $reason"
-                    }
-
-                    console.text = "Font Request Failed with reason:\n$r"
-                }
-            },
-            Handler(Looper.getMainLooper())
+            makeFontRequestCallback(font),
+            handler
         )
+    }
+
+    private fun getNextFont(): String {
+        val f = FONTS[fontIndex]
+        fontIndex++
+        if (fontIndex >= FONTS.size) {
+            fontIndex = 0
+        }
+        return f
+    }
+
+    private fun makeFontRequestCallback(font: String) : FontsContractCompat.FontRequestCallback {
+
+        return object : FontsContractCompat.FontRequestCallback() {
+            override fun onTypefaceRetrieved(typeface: Typeface) {
+                console.typeface = typeface
+                console.text = "Font Request for:\n$font\nSucceeded"
+            }
+
+            override fun onTypefaceRequestFailed(reason: Int) {
+                val r = when(reason) {
+                    FAIL_REASON_PROVIDER_NOT_FOUND -> "FAIL REASON PROVIDER NOT FOUND"
+                    FAIL_REASON_FONT_LOAD_ERROR -> "FAIL REASON FONT LOAD ERROR"
+                    FAIL_REASON_FONT_NOT_FOUND -> "FAIL REASON FONT NOT FOUND"
+                    FAIL_REASON_FONT_UNAVAILABLE -> "FAIL REASON FONT UNAVAILABLE"
+                    FAIL_REASON_MALFORMED_QUERY -> "FAIL REASON MALFORMED QUERY"
+                    FAIL_REASON_WRONG_CERTIFICATES -> "FAIL REASON SECURITY VIOLATION"
+                    FAIL_REASON_SECURITY_VIOLATION -> "FAIL REASON SECURITY VIOLATION"
+                    else -> "OTHER REASON $reason"
+                }
+
+                console.text = "Font Request for:\n$font\nFailed with reason:\n$r"
+            }
+        }
     }
 }
